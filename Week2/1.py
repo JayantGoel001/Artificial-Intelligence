3e1f55c7f9ebc365b6c717520cd13de061050120
username <54479676+CormacKrum@users.noreply.github.com>
2021-02-02 14:28:29 +0530

    Initial Commit

--- Week2/1.py
+++ Week2/1.py
@@ -1,6 +1,19 @@
 lines = []
 
-with open("Sample Text.txt",'r+') as f:
+with open("Sample Text.txt", 'r+') as f:
     lines = f.read().split("\n")
 
-print(lines)
+words = []
+
+for line in lines:
+    for word in line.split():
+        words.append(word)
+
+characters = []
+for word in words:
+    for character in word:
+        characters.append(character)
+
+print("Number of Lines :", len(lines))
+print("Number of Words :", len(words))
+print("Number of Characters :", len(characters))

6064d1c94a7b57a2b9e63fbe7507419ece92ef5d
JayantGoel001 <54479676+CormacKrum@users.noreply.github.com>
2021-03-18 01:11:57 +0530

    Initial Commit

--- Week6/2.py
+++ Week6/2.py
@@ -21,13 +21,46 @@ for key, value in candidates.items():
             candidates_reversed[lang] = []
         candidates_reversed[lang].append(key)
 
+print(candidates_reversed)
+def candidateHired(project_requirements, hired):
+    for hired_key, hired_value in hired.items():
+        for requirements in hired_value:
+            project_requirements[requirements] -= 1
+    return project_requirements
+
+
 hired = {
     'Anmol': ['C#']
 }
-for hired_key, hired_value in hired.items():
-    for requirements in hired_value:
-        project_requirements[requirements] -= 1
+print("Hired Candidates:\n")
+print(hired)
 
+numberOfCandidateToHire = 3
+# For Job based Variable, there are three jobs J1,J2 and J3 representing 3 openings.The domains of all three variables are initially all the people.
+# Then We can pose constraints which results in :
+print(candidateHired(project_requirements, hired))
+
+# Now we will use MRV(Minimum Remaining Value) and Degree heuristic
+d = {}
+for key, value in candidates_reversed.items():
+    d[key] = len(value)
+keys = sorted(d, key=d.get)
+
+sorted_dic = []
+for w in keys:
+    sorted_dic.append((w, candidates_reversed[w]))
+
+for i in range(numberOfCandidateToHire):
+    if i < len(sorted_dic):
+        domain = sorted_dic[i][0]
+        for can in sorted_dic[i][1]:
+            if can not in hired:
+                hired[can] = []
+            hired[can].append(domain)
+        if len(hired)>=3+1:
+            break
+
+print("Hired Candidates:\n")
+print(hired)
+project_requirements = candidateHired(project_requirements, hired)
 print(project_requirements)
-print(candidates_reversed)
-
